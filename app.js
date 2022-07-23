3b3283c7ac818a71449717bdc25c3dae336735dc
pranav s <dev.pranabsk@gmail.com>
2022-07-23 15:23:25 +0530

    Handling PUT Request

--- app.js
+++ app.js
@@ -47,6 +47,24 @@ app.get("/api/students/:id", (req, res) => {
     })
 })
 
+//PUT by id
+app.put("/api/students/:id", (req, res) => {
+    const id = parseInt(req.params.id);
+    const updatedData = req.body; 
+    db.getDbStudents()
+    .then(students => {
+        const student = students.find(s => s.id === id);
+        if(!student) res.status(404).send("No student found with this is!");
+        else {
+            const i = students.findIndex(s => s.id === id);
+            students[i] = updatedData;
+            db.insertDbStudents(students)
+            .then(msg => res.send(updatedData));
+        }
+    });
+})
+   
+
 const port = 3000;
 
 app.listen(port, () => {
