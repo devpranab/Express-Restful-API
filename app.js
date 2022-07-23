42423f199c8b110bbe4e21064274718a19581eed
pranav s <dev.pranabsk@gmail.com>
2022-07-23 20:55:20 +0530

    Third Party Middlewares

--- app.js
+++ app.js
@@ -2,12 +2,19 @@
 const express = require("express");
 const app = express();
 const studentRouter = require("./routers/studentRouter");
+const morgan = require("morgan");
 
 //middlewares
 app.use(express.json()); //POST/PUT/PATCH -> json object -> req.body
 
 app.use(express.urlencoded({extended: true})); //pass as key - value(Postmon)
 app.use(express.static('public')); //static file - http://localhost:3000/random.txt
+
+//Third Party Middlewares
+//app.use(morgan('dev')); //get req type,url as dev formar
+//app.use(morgan('tiny')); //get req type,url as tiny formar
+//app.use(morgan('short')); //get req type,url as short formar
+
 
 app.use((req, res, next) => {
     console.log("I am middleware 1");
