5ba0b3198a3b17c38e3bcc30bbed5c0b0b3e4982
pranav s <dev.pranabsk@gmail.com>
2022-07-23 08:24:39 +0530

    Creating server and Running with Nodemon

--- app.js
+++ app.js
@@ -1,15 +1,29 @@
-//Nodejs
-const http = require("http");
+//Use express
+const express = require("express");
+const app = express();
 
-const server = http.createServer((req, res) => {
-    if(req.url === "/"){
-        res.write("Hello World");
-        res.end();
-    }
+// app.get()
+// app.post()
+// app.patch()
+// app.delete()
+
+app.get("/", (req, res) => {
+    res.send("Hello from express js!");
+});
+
+app.get("/1", (req, res) => {
+    res.send("Hello from 1 express js!");
+});
+
+app.get("/2", (req, res) => {
+    res.send("Hello from 2 express js!");
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+
+    console.log(`Listening on port 3000`);
 })
 
-server.listen(3001);
-
-console.log("Listening on Port 3000...");
-
-//Now we are using express
+//nodemon app - run
