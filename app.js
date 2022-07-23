afe4f2e9ae1c92607462327cf438bdeda5a071ed
pranav s <dev.pranabsk@gmail.com>
2022-07-23 20:14:05 +0530

    Writing Custom Middleware

--- app.js
+++ app.js
@@ -5,10 +5,27 @@ const studentRouter = require("./routers/studentRouter");
 
 //middlewares
 app.use(express.json()); //POST/PUT/PATCH -> json object -> req.body
+app.use((req, res, next) => {
+    console.log("I am middleware 1");
+    next();
+});
+
+app.use((req, res, next) => {
+    console.log("I am middleware 2");
+    next();
+});
+
+
 app.use("/api/students", studentRouter);
+
+app.get("/", (req, res, next) => {
+    //res.send("Another response");
+    next();
+});
 
 
 app.get("/", (req, res) => {
+    console.log("I am get Request Middleware");
     res.send("Hello from express js!");
 });
 
