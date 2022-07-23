6f56d135e4f56c41c1c9e01fb0e8f88cbcad8faf
pranav s <dev.pranabsk@gmail.com>
2022-07-23 09:19:37 +0530

    Handling POST Request

--- app.js
+++ app.js
@@ -3,6 +3,9 @@ const express = require("express");
 const fs = require("fs");
 const app = express();
 
+//middlewares
+app.use(express.json()); //data process as json
+
 // app.get()
 // app.post()
 // app.patch()
@@ -12,12 +15,25 @@ app.get("/", (req, res) => {
     res.send("Hello from express js!");
 });
 
-app.get("/students", (req, res) => {
+app.get("/api/students", (req, res) => {
     fs.readFile("./database/db.json", "utf-8", (err, data) => {
         const students = JSON.parse(data).students;
         res.send(students);
     })
 });
+
+//POST
+app.post("/api/students", (req, res) => {
+    //console.log(req.body);
+    const student = req.body;
+    fs.readFile("./database/db.json", "utf-8", (err, data) => {
+        const students = JSON.parse(data);
+        students.students.push(student);
+      fs.writeFile("./database/db.json", JSON.stringify(students), (err) => {
+        res.send(student);
+      })
+    })
+})
 
 const port = 3000;
 
